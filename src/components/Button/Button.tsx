3fd1a4382a5c9e94d006f9094b84ff2ed9edd1a4
Sammy Anagolum <sammyanagolum@gmail.com>
2023-08-03 16:16:25 +0530

    Update 1

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,26 @@
+type ButtonProps = {
+    text: string
+    type: string
+    theme: string
+}
+
+
+
+export default function Button(props: ButtonProps) {
+    let buttonStyles: string;
+    buttonStyles = "px-5 py-2 rounded-md";
+
+    if (props.theme === "blue") {
+        buttonStyles += " text-white bg-midnightblue hover:bg-darkblue";
+    } else if (props.theme === "gray") {
+        buttonStyles += " text-black bg-white hover:bg-lightgrey";
+    }
+
+    return (
+        <div>
+            <button className={buttonStyles}>
+                {props.text}
+            </button>
+        </div>
+    )
+}
