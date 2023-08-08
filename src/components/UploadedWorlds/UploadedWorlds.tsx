6b4c1b9663b31c47a913ffcc9b1ce4f1d41c8d2f
Sashwat Anagolum <sashwat.anagolum@gmail.com>
2023-08-09 00:41:40 +0530

    update scripts

--- src/components/UploadedWorlds/UploadedWorlds.tsx
+++ src/components/UploadedWorlds/UploadedWorlds.tsx
@@ -4,7 +4,7 @@ export default function UploadedWorlds() {
     return (
         <div className="w-full flex flex-col gap-y-5 items-center">
             <p className="mx-5 text-lg font-semibold">Your Uploaded Worlds</p>
-            <WorldList></WorldList>
+            <WorldList user=""></WorldList>
         </div>
     );
 }
