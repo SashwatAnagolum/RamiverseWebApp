1eee8538accfe8e3a8fe4e05a75351a825025ba8
Sashwat Anagolum <sashwat.anagolum@gmail.com>
2023-08-09 00:38:06 +0530

    update scripts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="flex flex-col md:pl-12 pt-24 md:items-start items-center">
         <h1 className="md:pt-8 font-bold xl:text-8xl md:text-7xl sm:text-5xl text-4xl text-white max-w-fit">The Ramiverse</h1>
         <div className="mt-2">
-          <Button text="Explore Now" theme="blue" type="redirect"></Button>
+          <Button text="Explore Now" theme="blue" type="redirect" clickHandler={() => { }}></Button>
         </div>
       </div>
     </div>
