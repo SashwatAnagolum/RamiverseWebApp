edde4cd5addd3411d64f98d0ca7ad8f386a596e0
Sammy Anagolum <sammyanagolum@gmail.com>
2023-08-11 18:28:00 +0530

    Explore Now Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Button from '@/components/Button/Button';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -6,7 +7,9 @@ export default function Home() {
       <div className="flex flex-col md:pl-12 pt-24 md:items-start items-center">
         <h1 className="md:pt-8 font-bold xl:text-8xl md:text-7xl sm:text-5xl text-4xl text-white max-w-fit">The Ramiverse</h1>
         <div className="mt-2">
-          <Button text="Explore Now" theme="blue" type="redirect" clickHandler={null}></Button>
+          <Link href={'./publicworlds'}>
+            <Button text="Explore Now" theme="blue" type="redirect" clickHandler={null}></Button>
+          </Link>
         </div>
       </div>
     </div>
